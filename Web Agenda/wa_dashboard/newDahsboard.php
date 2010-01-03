cc945d2f4e4c85159a3319a7a90eca6c0269166f
markhazlett9 <markhazlett9@6628f44e-b90d-11de-ba48-051263943efc>
2010-01-03 21:51:32 +0000

    Starting a new Dashboard to accompany the prototypes made :D

---
+++ Web Agenda/wa_dashboard/newDahsboard.php
@@ -0,0 +1,45 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	<head>
+		<title>Web Agenda - Dashboard</title>
+		
+		<link type="text/css" href="../lib/development-bundle/themes/base/ui.all.css" rel="stylesheet" />
+		<link rel="stylesheet" href="dashCSS.css" type="text/css" />
+		
+		<script src="../lib/js/jquery-1.3.2.min.js" type="text/javascript" />
+		<script type="text/javascript" src="../lib/development-bundle/ui/ui.core.js"></script>
+  		<script type="text/javascript" src="../lib/development-bundle/ui/ui.datepicker.js"></script>
+	</head>
+	<body>
+		<div id="container">
+			<div id="header">
+				<div id="companyName">Deerfoot Inn and Casino</div>
+			</div>
+			<div id="sidebar">
+				<div id="sidebarSection">
+					<div id="sidebarHeader">First Header</div>
+					<div id="sidebarContent">
+						<ul>
+							<li>First Link</li>
+							<li>Second Link</li>
+							<li>Third Link</li>
+						</ul>
+					</div>
+				</div>
+				
+				<div id="sidebarSection">
+					<div id="sidebarHeader">First Header</div>
+					<div id="sidebarContent">
+						<ul>
+							<li>First Link</li>
+							<li>Second Link</li>
+							<li>Third Link</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div id="mainContentArea"></div>
+			<div id="footer"></div>
+		</div>
+	</body>
+</html>
