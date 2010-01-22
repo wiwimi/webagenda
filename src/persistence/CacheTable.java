a8649baa51bbed035e25fb3874c69a658bada40b
Initial.Dann <Initial.Dann@6628f44e-b90d-11de-ba48-051263943efc>
2010-01-22 21:35:27 +0000

    Added a doc and a log folder specifically for the java src/ folder to use. More work on Cache Table.

--- src/persistence/CacheTable.java
+++ src/persistence/CacheTable.java
@@ -3,6 +3,8 @@
  */
 package persistence;
 
+
+import java.util.Date;
 import utilities.DoubleLinkedList;
 
 /**
@@ -12,8 +14,15 @@ import utilities.DoubleLinkedList;
  */
 public class CacheTable {
 
+	/** Double Linked List --<br /><br /> 
+	 * Worst Case Scenario for finding a sorted ID in the cache table: (O) n / 2 <br />
+	 * Faster Inserting and Removing of objects, optimizing cache response time. */
 	private DoubleLinkedList<Cachable> cached_data 						= null;
 	
+	/** Date when last cache flush occured. If > than the specified routine flush time, Broker knows
+	 * that an error has occured such as a power failure upon request to flush. This should be
+	 * logged to an error log. */
+	private Date flush_stamp											= null;
 	
 	
 }
