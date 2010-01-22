4f02540f978e6304c017cb81afffdb7ec446ff9e
markhazlett9 <markhazlett9@6628f44e-b90d-11de-ba48-051263943efc>
2010-01-22 20:15:46 +0000

    added login servlet to UI package

---
+++ src/ui/Login.java
@@ -0,0 +1,83 @@
+package ui;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet(name="Login", urlPatterns={"/login"})
+public class Login extends HttpServlet 
+{
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() 
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        PrintWriter out = response.getWriter();
+        try 
+        {
+            ServletContext context = getServletContext();
+            RequestDispatcher dispatcher;
+
+            if(username.equals("admin") && password.equals("password"))
+            {
+                dispatcher = context.getRequestDispatcher("wa_dashboard/dashboard.jsp");
+                dispatcher.forward(request, response);
+            }
+            else
+            {
+                dispatcher = context.getRequestDispatcher("/index.jsp?error=You entered an invalid username or password");
+                dispatcher.forward(request, response);
+            }
+        } 
+        finally
+        {
+            out.close();
+        }
+    } 
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+
+	}
+
+}
