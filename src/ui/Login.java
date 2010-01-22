f2e566ca60247d0379ae978df4152fd6f04ba44b
danwehr <danwehr@ftml.net>
2010-01-22 21:45:39 +0000

    Fixed login bug. Need to use doPost() instead of processRequest()!

--- src/ui/Login.java
+++ src/ui/Login.java
@@ -37,6 +37,21 @@ public class Login extends HttpServlet
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
+    } 
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
         response.setContentType("text/html;charset=UTF-8");
         
         String username = request.getParameter("username");
@@ -67,22 +82,6 @@ public class Login extends HttpServlet
         {
             out.close();
         }
-    } 
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	{
-		// TODO Auto-generated method stub
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	{
-
 	}
 
 }
