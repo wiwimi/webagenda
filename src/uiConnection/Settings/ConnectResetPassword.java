e8784eae5f468247efbefe28e69b85c8a053eb5d
markhazlett9 <markhazlett9@6628f44e-b90d-11de-ba48-051263943efc>
2010-02-25 18:19:40 +0000

    UI Connection package addition

---
+++ src/uiConnection/Settings/ConnectResetPassword.java
@@ -0,0 +1,50 @@
+package uiConnection.Settings;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ConnectSettings
+ */
+public class ConnectResetPassword extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ConnectResetPassword() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		String oldPassword = (String) request.getAttribute("oldPass");
+		String newPass1 = (String) request.getAttribute("newPass1");
+		String newPass2 = (String) request.getAttribute("newPass2");
+		
+		if(!newPass1.equals(newPass2))
+		{
+			//the passwords do not match so redirect them back to the reset password screen with an error
+			response.sendRedirect("wa_settings/Password.jsp?error=The passwords entered do not match");
+		}
+		else
+		{
+			//The passwords do match so proceed with action to reset password
+		}
+	}
+
+}
