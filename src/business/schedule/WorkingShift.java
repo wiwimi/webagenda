c48f62f30dee8c4735d1a4419e93301c8c8456ff
Initial.Dann <Initial.Dann@6628f44e-b90d-11de-ba48-051263943efc>
2010-03-08 03:09:43 +0000

    Work on Working* classes and start on a TemplateBroker.

--- src/business/schedule/WorkingShift.java
+++ src/business/schedule/WorkingShift.java
@@ -5,6 +5,8 @@ package business.schedule;
 
 import java.sql.Time;
 
+import business.BusinessObject;
+
 
 /**
  * @author peon-dev
@@ -12,15 +14,61 @@ import java.sql.Time;
  *
  * @see business.schedule.Shift
  */
-public class WorkingShift {
+public class WorkingShift extends BusinessObject {
 	
-	/** Represents the ID of the user who will be working the shift represented in this class. */
-	private int employee_id									= -1;
-	private Shift work_shift								= null;
 	
-	public WorkingShift(Time start, Time end)
+	private int shift_id									= -1;
+	private int schedule_id									= -1;
+	private Time start_time									= null;
+	private Time end_time									= null;
+	
+	public WorkingShift(int schedule, int shift)
 		{
-		
+		this.schedule_id = schedule;
+		this.shift_id = shift;
+		// TODO: Make default time difference (15 min)
+
 		}
 
+	public WorkingShift(int schedule, int shift, Time start, Time end)
+	{
+		this(schedule, shift);
+		
+		// Need to check to make sure start and end aren't in reverse order, or produce a negative.
+		// Get absolute value of the two?
+		
+		
+		this.start_time = start;
+		this.end_time = end;
+	}
+
+	/**
+	 * @return the shift_id
+	 */
+	public int getShift_id() {
+		return shift_id;
+	}
+
+	/**
+	 * @return the schedule_id
+	 */
+	public int getSchedule_id() {
+		return schedule_id;
+	}
+
+	/**
+	 * @return the start_time
+	 */
+	public Time getStart_time() {
+		return start_time;
+	}
+
+	/**
+	 * @return the end_time
+	 */
+	public Time getEnd_time() {
+		return end_time;
+	}
+	
+	
 }
