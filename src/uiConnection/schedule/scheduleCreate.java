a1df62548cb103bcd9f2da390df415786f2b6e0c
markhazlett9 <markhazlett9@6628f44e-b90d-11de-ba48-051263943efc>
2010-04-03 23:57:34 +0000

    Added schedule template. It is almost working now :D

--- src/uiConnection/schedule/scheduleCreate.java
+++ src/uiConnection/schedule/scheduleCreate.java
@@ -5,6 +5,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import business.schedule.ScheduleTemplate;
+import business.schedule.ShiftTemplate;
 
 /**
  * Servlet implementation class scheduleCreate
@@ -30,8 +34,18 @@ public class scheduleCreate extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		HttpSession session = request.getSession();
+		
+		ScheduleTemplate sched = (ScheduleTemplate)session.getAttribute("schedule");
+		
+		if(request.getParameter("action").equals("addShift"))
+		{
+			ShiftTemplate shiftTemp = new ShiftTemplate();
+			
+			sched.getShiftTemplates().add(shiftTemp);
+		}
 	}
 
 }
--- src/uiConnection/schedule/scheduleTemplate.java
+++ src/uiConnection/schedule/scheduleTemplate.java
@@ -1,12 +1,26 @@
 package uiConnection.schedule;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import utilities.DoubleLinkedList;
+
+import business.schedule.ScheduleTemplate;
+import business.schedule.ShiftPosition;
+import business.schedule.ShiftTemplate;
 
 /**
+ * @author Mark Hazlett
  * Servlet implementation class scheduleTemplate
  */
 public class scheduleTemplate extends HttpServlet {
@@ -33,7 +47,58 @@ public class scheduleTemplate extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
-		
+		try
+		{
+			HttpSession session = request.getSession();
+			
+			ScheduleTemplate template = (ScheduleTemplate)session.getAttribute("schedule");
+			
+			String startTime = request.getParameter("startTime");
+			String endTime = request.getParameter("endTime");
+			String dayOfWeek = request.getParameter("dayOfWeek");
+			
+			int counter = Integer.parseInt(request.getParameter("counter"));
+			
+			DoubleLinkedList<ShiftPosition> listOfPositions = new DoubleLinkedList<ShiftPosition>();
+			
+			for(int index = 0; index < counter; index++)
+			{
+				
+				ShiftPosition position = new ShiftPosition();
+				position.setPosName(request.getParameter("positionType" + index));
+				position.setPosCount(Integer.parseInt(request.getParameter("positionNumber" + index)));
+				
+				listOfPositions.add(position);
+			}
+			
+			DateFormat sdf = new SimpleDateFormat("hh:mm");
+
+			try
+			{
+				ShiftTemplate shiftTemplate = new ShiftTemplate();
+				
+				Time startTimeParsed = (Time) sdf.parse(startTime);	
+				Time endTimeParsed = (Time)sdf.parse(endTime);
+				
+				shiftTemplate.setStartTime(startTimeParsed);
+				shiftTemplate.setEndTime(endTimeParsed);
+				shiftTemplate.setDay(Integer.parseInt(dayOfWeek));
+				
+				shiftTemplate.setShiftPositions(listOfPositions);
+				
+				template.getShiftTemplates().add(shiftTemplate);
+				
+			}
+			catch(Exception e)
+			{
+				//catch the date parse exception
+			}
+			
+		}
+		finally
+		{
+			response.sendRedirect("createShiftTemplate.jsp");
+		}
 	}
 
 }
