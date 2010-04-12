fd2123e3ba2d5ba83d4d76f9f72dd04e5ca7363d
danwehr <danwehr@ftml.net>
2010-04-12 20:02:45 +0000

    ConnectionManager now fetches connection information from an external text file when one is available.

--- src/application/Backup.java
+++ src/application/Backup.java
@@ -16,87 +16,89 @@ import java.util.Date;
  * 
  * @author Daniel Wehr
  * @version 0.1.0
- *
  */
 public class Backup
-	{
-	/**
-	 * Creates a new backup of the WebAgenda database and all information it
-	 * contains, using the given installation directory for the existing MySQL
-	 * server, and the target save directory.
-	 * 
-	 * @return a string containing the absolute path to the newly created sql
-	 *         file
-	 */
-	public static String backupDB()
-		{
-		File backupLocFile = new File("backupLocations.txt");
-		
-		if (!backupLocFile.exists())
-			throw new NullPointerException("Backup locations file has been removed.");
-		
-		File backupDir = null;
-		File sqlDir = null;
-		try
-			{
-			BufferedReader in = new BufferedReader(new FileReader(backupLocFile));
-			String backupDirString = in.readLine();
-			String sqlDirString = in.readLine();
-			
-			backupDir = new File(backupDirString.substring(backupDirString.indexOf(' ')+1,backupDirString.length()));
-			sqlDir = new File(sqlDirString.substring(sqlDirString.indexOf(' ')+1, sqlDirString.length()));
-			in.close();
-			}
-		catch (FileNotFoundException e1)
-			{
-			e1.printStackTrace();
-			}
-		catch (IOException e1)
-			{
-			e1.printStackTrace();
-			}
-		
-		//Get necessary parameters.
-		SimpleDateFormat format = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
-		Date now = new Date(System.currentTimeMillis());
-		String formatDate = format.format(now);
-		String DBName = ConnectionManager.getDb_name();
-		
-		//Ensure backup folder exists.
-		if (!backupDir.exists())
-			backupDir.mkdir();
-		
-		String filePath = backupDir+File.separator+formatDate+".sql";
-		String errorLogPath = backupDir+File.separator+"backupErrors.txt";
-		String command = "mysqldump --databases "+DBName+" -u WABroker -ppassword --single-transaction --skip-extended-insert --complete-insert --log-error="+errorLogPath+" --result-file="+filePath;
-		
-		Runtime rt = Runtime.getRuntime();
-		try
-			{
-			//Execute backup command.
-			Process pr = rt.exec(command, null, sqlDir);
-			
-			//Wait for backup to finish.
-			int exit = pr.waitFor();
-			
-			/*
-			 * Debug printouts. 
-			 * if (exit == 0)
-			 * 	System.out.println("Backup complete. New File: "+filePath);
-			 * else
-			 * 	System.out.println("Backup failed, code: "+exit);
-			 */
-			}
-		catch (IOException e)
-			{
-			e.printStackTrace();
-			}
-		catch (InterruptedException e)
-			{
-			e.printStackTrace();
-			}
-		
-		return filePath;
-		}
-	
-	}
+    {
+    /**
+     * Creates a new backup of the WebAgenda database and all information it
+     * contains, using the given installation directory for the existing MySQL
+     * server, and the target save directory.
+     * 
+     * @return a string containing the absolute path to the newly created sql
+     *         file
+     */
+    public static String backupDB()
+        {
+        File backupLocFile = new File("backupLocations.txt");
+        
+        if (!backupLocFile.exists())
+            throw new NullPointerException("Backup locations file has been removed.");
+        
+        File backupDir = null;
+        File sqlDir = null;
+        try
+            {
+            BufferedReader in = new BufferedReader(new FileReader(backupLocFile));
+            String backupDirString = in.readLine();
+            String sqlDirString = in.readLine();
+            
+            backupDir = new File(backupDirString.substring(backupDirString.indexOf(' ') + 1,
+                    backupDirString.length()));
+            sqlDir = new File(sqlDirString.substring(sqlDirString.indexOf(' ') + 1, sqlDirString
+                    .length()));
+            in.close();
+            }
+        catch (FileNotFoundException e1)
+            {
+            e1.printStackTrace();
+            }
+        catch (IOException e1)
+            {
+            e1.printStackTrace();
+            }
+        
+        // Get necessary parameters.
+        SimpleDateFormat format = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+        Date now = new Date(System.currentTimeMillis());
+        String formatDate = format.format(now);
+        String DBName = ConnectionManager.getDb_name();
+        
+        // Ensure backup folder exists.
+        if (!backupDir.exists())
+            backupDir.mkdir();
+        
+        String filePath = backupDir + File.separator + formatDate + ".sql";
+        String errorLogPath = backupDir + File.separator + "backupErrors.txt";
+        String command = "mysqldump --databases " +
+                DBName +
+                " -u WABroker -ppassword --single-transaction --skip-extended-insert --complete-insert --log-error=" +
+                errorLogPath + " --result-file=" + filePath;
+        
+        Runtime rt = Runtime.getRuntime();
+        try
+            {
+            // Execute backup command.
+            Process pr = rt.exec(command, null, sqlDir);
+            
+            // Wait for backup to finish.
+            int exit = pr.waitFor();
+            
+            /*
+             * Debug printouts. if (exit == 0)
+             * System.out.println("Backup complete. New File: "+filePath); else
+             * System.out.println("Backup failed, code: "+exit);
+             */
+            }
+        catch (IOException e)
+            {
+            e.printStackTrace();
+            }
+        catch (InterruptedException e)
+            {
+            e.printStackTrace();
+            }
+        
+        return filePath;
+        }
+    
+    }
--- src/application/ConnectionManager.java
+++ src/application/ConnectionManager.java
@@ -3,6 +3,11 @@
  */
 package application;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -14,162 +19,146 @@ import exception.DBDownException;
  * @version 0.1.0
  */
 public class ConnectionManager
-	{
-	/** Database user that accesses WebAgenda data. Defaults to WABroker. */
-	private static String	db_user				= "WABroker";
-	/** Password that is used for database user. */
-	private static String	db_pass				= "password";
-	/** Database table that holds WebAgenda data. Methods will fail if this is inaccurate. */
-	private static String	db_name				= "WebAgenda";
-	/** Driver for accessing tables and data. Defaults to MySQL driver included.  */
-	private static String	db_driver			= "com.mysql.jdbc.Driver";
-	/** Driver instance, holds an instance of the driver so memory isn't wasted re-creating
-	 * the instance whenever a connection is required. */
-	private static Object	db_drv_instance	= null;
-	/** Driver information string that determines port and host and  table name used in the
-	 * connectin */
-	private static String	db_url				= "jdbc:mysql://localhost:3306/" + db_name;
-	
-	/**
-	 * Returns an initialized connection instance wrapped in WebAgenda's
-	 * DBConnection object for use with Broker objects for connecting to database
-	 * and performing statements. Instances are controlled by Brokers so memory
-	 * usage doesn't spiral out of control when the database is it with many
-	 * connection requests.
-	 * 
-	 * @return DBConnection object -- holds a connection with availability
-	 *         settings.
-	 * @throws DBDownException If the database information is incorrect or
-	 *            connection cannot be made (Database daemon / process / service
-	 *            is not running
-	 */
-	public static DBConnection getConnection() throws DBDownException
-		{
-		Connection conn = null;
-		try
-			{
-			if (db_drv_instance == null)
-				{
-				db_drv_instance = Class.forName(db_driver).newInstance();
-				}
-			conn = DriverManager.getConnection(db_url+"?user="+db_user+"&password="+db_pass);
-			}
-		catch (CommunicationsException e)
-			{
-			throw new DBDownException("Database Down",e);
-			}
-		catch (InstantiationException e)
-			{
-			e.printStackTrace();
-			}
-		catch (IllegalAccessException e)
-			{
-			e.printStackTrace();
-			}
-		catch (ClassNotFoundException e)
-			{
-			e.printStackTrace();
-			}
-		catch (SQLException e)
-			{
-			e.printStackTrace();
-			}
-		
-		return new DBConnection(conn);
-		}
+    {
+    /**
+     * Database user that accesses WebAgenda data. Defaults to WABroker.
+     */
+    private static String  db_user         = "WABroker";
+    /**
+     * Password that is used for database user.
+     */
+    private static String  db_pass         = "password";
+    /**
+     * Database table that holds WebAgenda data. Methods will fail if this is
+     * inaccurate.
+     */
+    private static String  db_name         = "WebAgenda";
+    /**
+     * Driver for accessing tables and data. Defaults to MySQL driver included.
+     */
+    private static String  db_driver       = "com.mysql.jdbc.Driver";
+    /**
+     * Driver instance, holds an instance of the driver so memory isn't wasted
+     * re-creating the instance whenever a connection is required.
+     */
+    private static Object  db_drv_instance = null;
+    /**
+     * Driver information string that determines port and host and table name
+     * used in the connection
+     */
+    private static String  db_url          = "jdbc:mysql://localhost:3306/" + db_name;
+    /**
+     * Flag for whether or not the currently held connection settings are from
+     * the properties file.
+     */
+    private static boolean hasSettings     = false;
+    
+    /**
+     * Returns an initialized connection instance wrapped in WebAgenda's
+     * DBConnection object for use with Broker objects for connecting to
+     * database and performing statements. Instances are controlled by Brokers
+     * so memory usage doesn't spiral out of control when the database is it
+     * with many connection requests.
+     * 
+     * @return DBConnection object -- holds a connection with availability
+     *         settings.
+     * @throws DBDownException If the database information is incorrect or
+     *             connection cannot be made (Database daemon / process /
+     *             service is not running
+     */
+    public static DBConnection getConnection() throws DBDownException
+        {
+        Connection conn = null;
+        try
+            {
+            if (db_drv_instance == null)
+                db_drv_instance = Class.forName(db_driver).newInstance();
+            
+            if (!hasSettings)
+                getFileSettings();
+            
+            conn = DriverManager
+                    .getConnection(db_url + "?user=" + db_user + "&password=" + db_pass);
+            }
+        catch (CommunicationsException e)
+            {
+            throw new DBDownException("Database Down", e);
+            }
+        catch (InstantiationException e)
+            {
+            e.printStackTrace();
+            }
+        catch (IllegalAccessException e)
+            {
+            e.printStackTrace();
+            }
+        catch (ClassNotFoundException e)
+            {
+            e.printStackTrace();
+            }
+        catch (SQLException e)
+            {
+            e.printStackTrace();
+            }
+        
+        return new DBConnection(conn);
+        }
+    
+    /**
+     * @return the db_name
+     */
+    public static String getDb_name()
+        {
+        return db_name;
+        }
 
-	/**
-	 * Set the database user
-	 * @param dbUser the db_user to set
-	 */
-	protected static void setDb_user(String dbUser) {
-		db_user = dbUser;
-	}
-
-	/**
-	 * Set the database user password
-	 * @param dbPass the db_pass to set
-	 */
-	protected static void setDb_pass(String dbPass) {
-		db_pass = dbPass;
-	}
-
-	/**
-	 * Set the database instance name
-	 * @param dbName the db_name to set
-	 */
-	protected static void setDb_name(String dbName) {
-		db_name = dbName;
-	}
-
-	/**
-	 * Set the driver library name to use 
-	 * @param dbDriver the db_driver to set
-	 */
-	protected static void setDb_driver(String dbDriver) {
-		db_driver = dbDriver;
-	}
-
-	/**
-	 * Sets the database url (hostname, port, and database instance; should use the 
-	 * getDb_name() method for db instance name appended to url)
-	 * @param dbUrl the db_url to set
-	 */
-	protected static void setDb_url(String dbUrl) {
-		db_url = dbUrl;
-	}
-
-	/**
-	 * Gets the Database user
-	 * @return the db_user
-	 */
-	protected static String getDb_user() {
-		return db_user;
-	}
-
-	/**
-	 * Gets the Database user's password 
-	 * @return the db_pass
-	 */
-	protected static String getDb_pass() {
-		return db_pass;
-	}
-
-	/**
-	 * Gets the database instance name
-	 * @return the db_name
-	 */
-	protected static String getDb_name() {
-		return db_name;
-	}
-
-	/**
-	 * Gets the database connection driver name 
-	 * @return the db_driver
-	 */
-	protected static String getDb_driver() {
-		return db_driver;
-	}
-
-	/**
-	 * Gets the url of the database (includes hostname, port, and db instance name)
-	 * @return the db_url
-	 */
-	protected static String getDb_url() {
-		return db_url;
-	}
-	
-	/**
-	 * Simple system.out test depicting whether Properties file meets the default
-	 * values set. A false printout will determine that the value has been changed (is custom)
-	 * so functionality may vary if a value is incorrect.
-	 */
-	protected static void printTest() 
-	{
-		System.out.println("User: " + (getDb_user().equals("WABroker")));
-		System.out.println("Pass: " + (getDb_pass().equals("password")));
-		System.out.println("Name: " + (getDb_name().equals("WebAgenda")));
-		System.out.println("Driver: " + (getDb_driver().equals("com.mysql.jdbc.Driver")));
-		System.out.println("URL: " + (getDb_url().equals("jdbc:mysql://localhost:3306/" + db_name)));
-	}
-	}
+    /**
+     * Attempts to get connection configuration information from an external
+     * properties file.
+     */
+    private static void getFileSettings()
+        {
+        File file = new File("Properties.txt");
+        
+        if (!hasSettings && file.exists())
+            {
+            try
+                {
+                BufferedReader bw = new BufferedReader(new FileReader(file));
+                db_user = bw.readLine();
+                db_pass = bw.readLine();
+                db_name = bw.readLine();
+                db_driver = bw.readLine();
+                db_url = bw.readLine() + db_name;
+                bw.close();
+                
+                // System.out.println("Copied settings from file.");
+                hasSettings = true;
+                }
+            catch (FileNotFoundException e)
+                {
+                e.printStackTrace();
+                }
+            catch (IOException e)
+                {
+                e.printStackTrace();
+                }
+            }
+        }
+    
+    /**
+     * Simple system.out test depicting whether Properties file meets the
+     * default values set. A false printout will determine that the value has
+     * been changed (is custom) so functionality may vary if a value is
+     * incorrect.
+     */
+    protected static void printTest()
+        {
+        System.out.println("User: " + (db_user.equals("WABroker")));
+        System.out.println("Pass: " + (db_pass.equals("password")));
+        System.out.println("Name: " + (db_name.equals("WebAgenda")));
+        System.out.println("Driver: " + (db_driver.equals("com.mysql.jdbc.Driver")));
+        System.out
+                .println("URL: " + (db_url.equals("jdbc:mysql://localhost:3306/" + db_name)));
+        }
+    }
