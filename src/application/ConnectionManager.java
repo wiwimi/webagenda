2c184eab00fd28cb78c77da773656179557ca72b
danwehr <danwehr@ftml.net>
2010-04-13 21:34:52 +0000

    Backup and Connection manager now look for config files in the directory C:\WebAgendaConfig\

--- src/application/ConnectionManager.java
+++ src/application/ConnectionManager.java
@@ -121,7 +121,7 @@ public class ConnectionManager
      */
     private static void getFileSettings()
         {
-        File file = new File("Properties.txt");
+        File file = new File("C:/WebAgendaConfig/Properties.txt");
         
         if (!hasSettings && file.exists())
             {
