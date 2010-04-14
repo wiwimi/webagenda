438e3327222b4cf974089bcef712beeb43c7c876
noorin671 <noorin671@6628f44e-b90d-11de-ba48-051263943efc>
2010-04-14 01:33:10 +0000

    Changed one of the help messages.

--- WebContent/lib/js/helpBackup.js
+++ WebContent/lib/js/helpBackup.js
@@ -3,7 +3,7 @@
 
 $(document).ready(function() {
 $('#helpIcon').click(function() {
-	$.prompt("* This window is used for backing up the system as well as importing existing backup files. <br /> " +
+	$.prompt("* This window is used for backing up the system. <br /> " +
 			"* The procedure is smooth and will not interfere in user's work.<br /> " +
 			"* Do not worry about shutting down the databse while backing-up.<br /> " +
 			"* For more information, please refer to the Online Help.");
